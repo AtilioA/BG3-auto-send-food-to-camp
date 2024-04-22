63daf0a4b748177cd0f62cd0e2716744f62f1ead
AtilioA <27153776+AtilioA@users.noreply.github.com>
2024-04-22 10:19:44 -0300

    feat: add new options to Config file: min amount to keep and min rarity to send

--- Auto Send Food To Camp/Mods/AutoSendFoodToCamp/ScriptExtender/Lua/Server/Helpers/Config.lua
+++ Auto Send Food To Camp/Mods/AutoSendFoodToCamp/ScriptExtender/Lua/Server/Helpers/Config.lua
@@ -1,37 +1,39 @@
 Config = VCHelpers.Config:New({
-  folderName = "AutoSendFoodToCamp",
-  configFilePath = "auto_send_food_to_camp_config.json",
-  defaultConfig = {
-    GENERAL = {
-      enabled = true, -- Toggle the mod on/off
+    folderName = "AutoSendFoodToCamp",
+    configFilePath = "auto_send_food_to_camp_config.json",
+    defaultConfig = {
+        GENERAL = {
+            enabled = true, -- Toggle the mod on/off
+        },
+        FEATURES = {
+            move_food = true,    -- Move food to the camp chest
+            move_beverages = true, -- Move beverages to the camp chest
+            move_bought_food = true, -- Move food bought from merchants to the camp chest
+            minimum_food_to_keep = 0, -- Minimum amount of food to keep in the party's inventory, 0 = send all food to the camp chest
+            minimum_rarity = "Common", -- Minimum rarity of food to keep in the party's inventory ("Common", "Uncommon", "Rare", "Epic", "Legendary")
+            -- move_stolen_food = false,    -- Move stolen food (right after stealing) to the camp chest
+            send_existing_food = {
+                enabled = true,    -- Move existing food in the party's inventory to the camp chest
+                nested_containers = true, -- Move food in nested containers (e.g. backpacks, supply sacks) to the camp chest
+                create_supply_sack = true, -- Move food to a supply sack inside the chest. It will be created if it doesn't exist.
+            },
+            ignore = {
+                healing = true, -- Ignore healing items (e.g. Goodberry)
+                weapons = false, -- Ignore weapons (only Salami in the vanilla base game)
+                user_defined = true, -- Ignore items defined by the user (in the user_ignored_food_list.json file)
+                wares = true,
+            }
+        },
+        DEBUG = {
+            level = 0 -- 0 = no debug, 1 = minimal, 2 = verbose debug logs
+        }
     },
-    FEATURES = {
-      move_food = true,        -- Move food to the camp chest
-      move_beverages = true,   -- Move beverages to the camp chest
-      move_bought_food = true, -- Move food bought from merchants to the camp chest
-      -- move_stolen_food = false,    -- Move stolen food (right after stealing) to the camp chest
-      send_existing_food = {
-        enabled = true,            -- Move existing food in the party's inventory to the camp chest
-        nested_containers = true,  -- Move food in nested containers (e.g. backpacks, supply sacks) to the camp chest
-        create_supply_sack = true, -- Move food to a supply sack inside the chest. It will be created if it doesn't exist.
-      },
-      ignore = {
-        healing = true,      -- Ignore healing items (e.g. Goodberry)
-        weapons = false,     -- Ignore weapons (only Salami in the vanilla base game)
-        user_defined = true, -- Ignore items defined by the user (in the user_ignored_food_list.json file)
-        wares = true,
-      }
-    },
-    DEBUG = {
-      level = 0 -- 0 = no debug, 1 = minimal, 2 = verbose debug logs
-    }
-  },
 })
 
 Config:UpdateCurrentConfig()
 
 Config:AddConfigReloadedCallback(function(configInstance)
-  ASFTCPrinter.DebugLevel = configInstance:GetCurrentDebugLevel()
-  ASFTCPrint(0, "Config reloaded: " .. Ext.Json.Stringify(configInstance:getCfg(), { Beautify = true }))
+    ASFTCPrinter.DebugLevel = configInstance:GetCurrentDebugLevel()
+    ASFTCPrint(0, "Config reloaded: " .. Ext.Json.Stringify(configInstance:getCfg(), { Beautify = true }))
 end)
 Config:RegisterReloadConfigCommand("asftc")
