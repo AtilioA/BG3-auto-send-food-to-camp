689c13455fa6d9aae087f235586cf91e3bfb713a
Atílio <27153776+AtilioA@users.noreply.github.com>
2024-05-15 15:56:45 -0300

    refactor: add condition wrapper to event listener

--- Auto Send Food To Camp/Mods/AutoSendFoodToCamp/ScriptExtender/Lua/Server/SubscribedEvents.lua
+++ Auto Send Food To Camp/Mods/AutoSendFoodToCamp/ScriptExtender/Lua/Server/SubscribedEvents.lua
@@ -1,47 +1,53 @@
 SubscribedEvents = {}
 
 function SubscribedEvents.SubscribeToEvents()
-    if true then
-        ASFTCPrint(2,
-            "Subscribing to events with JSON config: " ..
-            Ext.Json.Stringify(Mods.BG3MCM.MCMAPI:GetAllModSettings(ModuleUUID), { Beautify = true }))
-
-        Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "before", EHandlers.OnLevelGameplayStarted)
-        Ext.Osiris.RegisterListener("TimerFinished", 1, "before", EHandlers.OnTimerFinished)
-
-        -- Moving/looting
-        Ext.Osiris.RegisterListener("MovedFromTo", 4, "before", EHandlers.OnMovedFromTo)
-
-
-        -- Item pickup
-        Ext.Osiris.RegisterListener("RequestCanPickup", 3, "after", EHandlers.OnRequestCanPickup)
-        Ext.Osiris.RegisterListener("PickupFailed", 2, "after", EHandlers.OnPickupFailed)
-
-        if MCMGet('send_existing_food') then
-            Ext.Osiris.RegisterListener("TeleportedToCamp", 1, "before", EHandlers.OnTeleportedToCamp)
+    local function conditionalWrapper(handler)
+        return function(...)
+            if MCMGet("mod_enabled") then
+                handler(...)
+            else
+                ASFTCDebug(1, "Event handling is disabled.")
+            end
         end
-
-
-        -- Used to detect camp chest usage
-        Ext.Osiris.RegisterListener("UseStarted", 2, "before", EHandlers.OnUseStarted)
-        Ext.Osiris.RegisterListener("UseFinished", 3, "before", EHandlers.OnUseEnded)
-
-        -- TODO:
-        -- CharacterStoleItem
-        -- CharacterPickpocketSuccess
-
-        -- Trading (not necessary because of MovedFromTo?)
-        -- Ext.Osiris.RegisterListener("TradeEnds", 2, "before", EHandlers.OnTradeEnds)
-
-        -- Do not use:
-        -- Ext.Osiris.RegisterListener("TemplateOpening", 3, "before", EHandlers.OnTemplateOpening)
-        -- Ext.Osiris.RegisterListener("Moved", 1, "before", EHandlers.OnMoved)
-
-        -- Ext.Osiris.RegisterListener("DroppedBy", 2, "before", EHandlers.OnDroppedBy)
-        -- Ext.Osiris.RegisterListener("PreMovedBy", 2, "after", EHandlers.OnPreMovedBy)
-        -- Ext.Osiris.RegisterListener("CharacterStoleItem", 10, "before", EHandlers.OnCharacterStoleItem)
-        -- Ext.Osiris.RegisterListener("CharacterPickpocketSuccess", 10, "before", EHandlers.OnCharacterPickpocketSuccess)
     end
+
+    ASFTCPrint(2,
+        "Subscribing to events with JSON config: " ..
+        Ext.Json.Stringify(Mods.BG3MCM.MCMAPI:GetAllModSettings(ModuleUUID), { Beautify = true }))
+
+    Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "before", conditionalWrapper(EHandlers.OnLevelGameplayStarted))
+    Ext.Osiris.RegisterListener("TimerFinished", 1, "before", conditionalWrapper(EHandlers.OnTimerFinished))
+
+    -- Moving/looting
+    Ext.Osiris.RegisterListener("MovedFromTo", 4, "before", conditionalWrapper(EHandlers.OnMovedFromTo))
+
+    -- Item pickup
+    Ext.Osiris.RegisterListener("RequestCanPickup", 3, "after", conditionalWrapper(EHandlers.OnRequestCanPickup))
+    Ext.Osiris.RegisterListener("PickupFailed", 2, "after", conditionalWrapper(EHandlers.OnPickupFailed))
+
+    if MCMGet('send_existing_food') then
+        Ext.Osiris.RegisterListener("TeleportedToCamp", 1, "before", conditionalWrapper(EHandlers.OnTeleportedToCamp))
+    end
+
+    -- Used to detect camp chest usage
+    Ext.Osiris.RegisterListener("UseStarted", 2, "before", conditionalWrapper(EHandlers.OnUseStarted))
+    Ext.Osiris.RegisterListener("UseFinished", 3, "before", conditionalWrapper(EHandlers.OnUseEnded))
+
+    -- TODO:
+    -- CharacterStoleItem
+    -- CharacterPickpocketSuccess
+
+    -- Trading (not necessary because of MovedFromTo?)
+    -- Ext.Osiris.RegisterListener("TradeEnds", 2, "before", EHandlers.OnTradeEnds)
+
+    -- Do not use:
+    -- Ext.Osiris.RegisterListener("TemplateOpening", 3, "before", EHandlers.OnTemplateOpening)
+    -- Ext.Osiris.RegisterListener("Moved", 1, "before", EHandlers.OnMoved)
+
+    -- Ext.Osiris.RegisterListener("DroppedBy", 2, "before", EHandlers.OnDroppedBy)
+    -- Ext.Osiris.RegisterListener("PreMovedBy", 2, "after", EHandlers.OnPreMovedBy)
+    -- Ext.Osiris.RegisterListener("CharacterStoleItem", 10, "before", EHandlers.OnCharacterStoleItem)
+    -- Ext.Osiris.RegisterListener("CharacterPickpocketSuccess", 10, "before", EHandlers.OnCharacterPickpocketSuccess)
 end
 
 return SubscribedEvents
